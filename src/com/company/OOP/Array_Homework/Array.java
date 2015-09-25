7bc8f9603aea8d304786e7586b386f6d7d73ae49
MaksimSokur <maksim.sokur90@gmail.com>
2015-09-25 21:38:03 +0300

    1

--- src/com/company/OOP/Array_Homework/Array.java
+++ src/com/company/OOP/Array_Homework/Array.java
@@ -1,5 +1,7 @@
 package com.company.OOP.Array_Homework;
 
+import java.util.Random;
+
 /**
  * Created by User on 23.09.2015.
  */
@@ -8,13 +10,36 @@ public class Array {
 
     public int maximum (int array[]){
         int result = 0;
-
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+            System.out.print(" " + array[i]);
+        }
         for(int i = 0; i < array.length; i++)
         {
             if (result < array[i]) {
                 result = array[i];
             }
-        }return result;
+        }
+        System.out.println("\nMaximum in array is: " + result);
+        return result;
     }
+    public int minimum (int array[]){
+        int result = 0;
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+            System.out.print(" " + array[i]);
+        }
+        for(int i = 0; i < array.length; i++)
+        {
+            if (result > array[i]) {
+                result = array[i];
+            }
+        }
+        System.out.println("\nMinimum in array is: " + result);
+        return result;
+    }
+
 
 }
--- src/com/company/OOP/Array_Homework/main.java
+++ src/com/company/OOP/Array_Homework/main.java
@@ -1,15 +1,14 @@
 package com.company.OOP.Array_Homework;
 
-import java.util.Random;
-
 /**
  * Created by User on 23.09.2015.
  */
 public class main {
     public static void main(String[] args) {
-        Array array[] = new Array[5];
-        Random random = new Random();
-        int i = random.nextInt(array);
-    }
+        Array array = new Array();
+        int [] arr = new  int [10];
+        array.maximum(arr);
+        array.minimum(arr);
+
     }
 }
